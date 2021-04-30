194fb8624685264af42ef23ca4b15ca5af8c1d9c
agarcluq <agarcluq@everis.com>
2021-04-30 08:14:50 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React from "react";
-import "./style.css";
+import React, { useState, useEffect } from 'react';
 
-export default function App() {
+function App() {
+  const [isOn, setIsOn] = useState(false);
+  const [timer, setTimer] = useState(0);
+
+  useEffect(
+    () => {
+      let interval;
+
+      if (isOn) {
+        interval = setInterval(
+          () => setTimer(timer => timer + 1),
+          1000,
+        );
+      }
+
+      return () => clearInterval(interval);
+    },
+    [isOn],
+  );
+
+  const onReset = () => {
+    setIsOn(false);
+    setTimer(0);
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      {timer}
+
+      {!isOn && (
+        <button type="button" onClick={() => setIsOn(true)}>
+          Start
+        </button>
+      )}
+
+      {isOn && (
+        <button type="button" onClick={() => setIsOn(false)}>
+          Stop
+        </button>
+      )}
+
+      <button type="button" disabled={timer === 0} onClick={onReset}>
+        Reset
+      </button>
     </div>
   );
 }
+
+export default App;
